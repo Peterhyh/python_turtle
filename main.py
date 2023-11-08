cc446827b993891f3c13eb1002a1c30c143a66e3
Peter Huynh <peterhyh@yahoo.com>
2023-11-08 14:14:02 -0600

    turtle drawing a square

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ turtle = Turtle()
 turtle.shape("turtle")
 turtle.color("#DAA520")
 turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
 
 
 screen = Screen()
