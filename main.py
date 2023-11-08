d24727906f37e726498d5886ebbf7f12f71a5025
Peter Huynh <peterhyh@yahoo.com>
2023-11-08 16:32:43 -0600

    dash line

--- main.py
+++ main.py
@@ -5,13 +5,29 @@ turtle = Turtle()
 
 turtle.shape("turtle")
 turtle.color("#DAA520")
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+
+
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+
+# Easier way to move the turtle in a square:
+# for _ in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+
+
+# Draw a horzional dashed line:
+# for i in range(15):
+#     turtle.forward(10)
+#     if i % 2 == 0:
+#         turtle.penup()
+#     else:
+#         turtle.pendown()
 
 
 screen = Screen()
