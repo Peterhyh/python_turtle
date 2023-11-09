ff2968e5d43a18fd71fb0a5da4e3e264ad03c963
Peter Huynh <peterhyh@yahoo.com>
2023-11-08 18:21:54 -0600

    color changing shapes

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from turtle import Turtle, Screen
-
+import random
 
 turtle = Turtle()
 
 turtle.shape("turtle")
-turtle.color("#DAA520")
 
 
 # turtle.forward(100)
@@ -28,6 +27,20 @@ turtle.color("#DAA520")
 #         turtle.penup()
 #     else:
 #         turtle.pendown()
+
+
+# Increase the side of the shape by 1 and change the lines to a random color each iteration:
+arr = ["orange", "red", "blue", "green", "purple",
+       "pink", "yellow", "#ADFF2F", "#8A2BE2", "#FF7F50"]
+
+for i in range(3, 11):
+    ran_num = random.randint(0, 9)
+    turtle.color(arr[ran_num])
+    int_ang = (180 * i - 360)/i
+    tilt_ang = 180 - int_ang
+    for _ in range(i):
+        turtle.forward(100)
+        turtle.right(tilt_ang)
 
 
 screen = Screen()
